368b7da8e9ea52cedae6d3130cdfe71dd65f0971
gurkerl83 <markus_gritsch@gmx.de>
2020-05-24 20:22:38 +0200

    Add OSGi-aware configuration, refresh bundle hosts
    Configurations are OSGi-compatible, and directly integrated into the feature definition file. Apache Karaf knows how to access them, a file in the Apache Karafs /etc folder gets created.
    
    Important:
    Due to shortcomings in the Apache Felix runtime, fragment-hosts get not refreshed on restarting Apache Karaf instance. The refresher feature handles this situation.
    
    Small cleanup to avoid unnecessary OSGi configurations.

---
+++ demo-model/src/main/java/org/neo4j/ogm/demo/osgi/model/second/SecondProduct.java
@@ -0,0 +1,10 @@
+package org.neo4j.ogm.demo.osgi.model.second;
+
+import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.demo.osgi.model.Entity;
+
+@NodeEntity
+public class SecondProduct extends Entity {
+
+  public SecondProduct() {}
+}
